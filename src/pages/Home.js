97e048c0c5d51cc9fd392a3073f4d870a3d56737
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-05-22 21:48:02 +0100

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,36 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
+import { db } from "../firebase-config";
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [postList, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
 
-export default Home
+    getPosts();
+  });
+
+  const deletePost = async (id) => {
+    const postDoc = doc(db, "posts", id);
+    await deleteDoc(postDoc);
+  };
+  return (
+    <div>
+      {postList.map((post) => {
+        return (
+          <div>
+            <div className="post" key={post.id}>
+              {post.title}
+            </div>
+            <button onClick={() => deletePost(post.id)}>Delete</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Home;
