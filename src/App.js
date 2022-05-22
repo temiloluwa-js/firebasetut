b18ba563d0309bbf6cb69fc2366efbd579effc4c
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-05-22 17:58:02 +0100

    created routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 
 import './App.css';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import CreatePost from './pages/CreatePost'
+import Login from './pages/Login'
 function App() {
   return (
-    <div className="App">
 
-    </div>
-  );
+    <Router>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/createpost'>Create Post</Link>
+        <Link to='/login'>Login</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/createpost' element={<CreatePost/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </Router>
+    );
 }
 
 export default App;
